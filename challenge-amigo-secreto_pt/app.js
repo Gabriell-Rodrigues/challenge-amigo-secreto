d9a32c1435f315ac96ae58035da4d8a21a7c7202
Gabriel Rodrigues <gabriel319r@gmail.com>
2025-01-25 21:17:09 -0300

    Correções, e criação de novas funções

--- challenge-amigo-secreto_pt/app.js
+++ challenge-amigo-secreto_pt/app.js
@@ -1,6 +1,3 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação.
-// Aqui você deverá desenvolver a lógica para resolver o problema.
-
 let amigos = [];
 
 function adicionarAmigo() {
@@ -11,10 +8,34 @@ function adicionarAmigo() {
     amigos.push(nomes);
   }
   limparCampo();
+  percorrendoArrey();
+  sortearAmigo();
   console.log(amigos);
 }
 
 function limparCampo() {
   let campo = document.querySelector("#amigo");
   campo.value = "";
+}
+
+function percorrendoArrey() {
+  let lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+
+  for (let i = 0; i < amigos.length; i++) {
+    const li = document.createElement("li");
+    li.textContent = amigos[i];
+    lista.appendChild(li);
+  }
+}
+
+function sortearAmigo() {
+  if (amigos.length > 0) {
+    const indiceSorteado = Math.floor(Math.random() * amigos.length);
+    const sorteado = amigos[indiceSorteado];
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = `${sorteado}`;
+  } else {
+    alert("Ainda não foram adicionados os amigos");
+  }
 }
